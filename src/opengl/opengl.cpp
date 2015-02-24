290aa38f4ce775628abf70dc89c78bf44ca3a43e
ruslashev <ruslashev@gmail.com>
2015-02-24 21:34:10 +0300

    Work on planets more, it should actually work now

--- src/opengl/opengl.cpp
+++ src/opengl/opengl.cpp
@@ -3,23 +3,19 @@
 void generatePlanet(std::vector<GLfloat> *vertices,
 		std::vector<GLushort> *elements)
 {
-	static unsigned short element_counter = 0;
 	struct v3 {
 		float x, y, z;
-		unsigned short id;
 		v3(float nx, float ny, float nz) {
 			x = nx;
 			y = ny;
 			z = nz;
-			id = element_counter++;
-			printf("create id %d (%f %f %f)\n", id, x, y, z);
 		};
 		v3 avg(const v3 &b) const {
 			const float nx = (x + b.x)/2;
 			const float ny = (y + b.y)/2;
 			const float nz = (z + b.z)/2;
 			v3 nv(nx, ny, nz);
-			nv.normalize();
+			// nv.normalize();
 			return nv;
 		}
 		v3 operator/(const float &r) { return v3(x/r, y/r, z/r); }
@@ -35,7 +31,7 @@ void generatePlanet(std::vector<GLfloat> *vertices,
 		}
 	};
 	struct triangle {
-		v3 f, s, t;
+		unsigned int f, s, t;
 		bool dirty;
 	};
 
@@ -51,64 +47,77 @@ void generatePlanet(std::vector<GLfloat> *vertices,
 	const v3 B = {    0,  0,  b };
 	const v3 C = {  a/2,  0, -s };
 
-	triangles.push_back({D,A,C, false});
-	triangles.push_back({D,C,B, false});
-	triangles.push_back({D,A,B, false});
-	triangles.push_back({A,B,C, false});
+	std::vector<v3> used_vertices;
+	used_vertices.push_back(D);
+	used_vertices.push_back(A);
+	used_vertices.push_back(B);
+	used_vertices.push_back(C);
 
-	for (auto &t : triangles) {
+	triangles.push_back({0,1,3, false});
+	triangles.push_back({0,3,2, false});
+	triangles.push_back({0,1,2, false});
+	triangles.push_back({1,2,3, false});
+
+	const size_t triangles_len = triangles.size();
+	for (size_t i = 0; i < triangles_len; i++) {
+		const triangle t = triangles[i];
 		if (t.dirty)
 			continue;
 
-		v3 midp_fs = t.f.avg(t.s);
-		v3 midp_ft = t.f.avg(t.t);
-		v3 midp_st = t.s.avg(t.t);
+		// if you want to understand the code below,
+		// grab a pen & paper
 
-		triangle new_tri { midp_fs, midp_ft, midp_st, true };
+		const unsigned int f_i = t.f;
+		const unsigned int s_i = t.s;
+		const unsigned int t_i = t.t;
 
-		triangles.push_back(new_tri);
+		v3 f_v = used_vertices[f_i];
+		v3 s_v = used_vertices[s_i];
+		v3 t_v = used_vertices[t_i];
+
+		const v3 midp_fs = f_v.avg(s_v);
+		const v3 midp_ft = f_v.avg(t_v);
+		const v3 midp_st = s_v.avg(t_v);
+
+		used_vertices.push_back(midp_fs);
+		const unsigned int fs_i = used_vertices.size()-1;
+		used_vertices.push_back(midp_ft);
+		const unsigned int ft_i = used_vertices.size()-1;
+		used_vertices.push_back(midp_st);
+		const unsigned int st_i = used_vertices.size()-1;
+
+		triangle new_f_ft_fs { f_i, ft_i, fs_i, true };
+		triangle new_s_fs_st { s_i, fs_i, st_i, true };
+		triangle new_t_st_ft { t_i, st_i, ft_i, true };
+		triangle new_fs_st_ft { fs_i, st_i, ft_i, true };
+
+		// todo: is dirty even needed?
+		triangles.erase(triangles.begin()+i);
+		triangles.insert(triangles.begin()+i, new_f_ft_fs);
+		triangles.insert(triangles.begin()+i, new_s_fs_st);
+		triangles.insert(triangles.begin()+i, new_t_st_ft);
+		triangles.insert(triangles.begin()+i, new_fs_st_ft);
+		i += 4;
+		i--; // because of the increment in for loop
 	}
 	for (size_t i = 0; i < triangles.size(); i++)
 		triangles[i].dirty = false;
 
-	std::vector<GLfloat> lv; // local_vertices
-	std::vector<GLushort> le; // local_elements
-	std::vector<bool> pushed;
-	pushed.resize(triangles.size()*3, 0);
+	std::vector<GLfloat> local_vertices;
+	std::vector<GLushort> local_elements;
+	for (auto &v : used_vertices) {
+		local_vertices.push_back(v.x);
+		local_vertices.push_back(v.y);
+		local_vertices.push_back(v.z);
+	}
 	for (auto &t : triangles) {
-		if (!pushed[t.f.id]) {
-			pushed[t.f.id] = true;
-			lv.push_back(t.f.x);
-			lv.push_back(t.f.y);
-			lv.push_back(t.f.z);
-			le.push_back(t.f.id);
-		} else {
-			le.push_back(t.f.id);
-		}
-
-		if (!pushed[t.s.id]) {
-			pushed[t.s.id] = true;
-			lv.push_back(t.s.x);
-			lv.push_back(t.s.y);
-			lv.push_back(t.s.z);
-			le.push_back(t.s.id);
-		} else {
-			le.push_back(t.s.id);
-		}
-
-		if (!pushed[t.t.id]) {
-			pushed[t.t.id] = true;
-			lv.push_back(t.t.x);
-			lv.push_back(t.t.y);
-			lv.push_back(t.t.z);
-			le.push_back(t.t.id);
-		} else {
-			le.push_back(t.t.id);
-		}
+		local_elements.push_back(t.f);
+		local_elements.push_back(t.s);
+		local_elements.push_back(t.t);
 	}
 
-	*vertices = lv;
-	*elements = le;
+	*vertices = local_vertices;
+	*elements = local_elements;
 }
 
 void OpenGL::Construct(const unsigned int window_width,
@@ -161,7 +170,7 @@ void OpenGL::Draw()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	element_buffer.Bind();
-	glDrawElements(GL_LINE_LOOP, temp_elements_size, GL_UNSIGNED_SHORT, 0);
+	glDrawElements(GL_POINTS, temp_elements_size, GL_UNSIGNED_SHORT, 0);
 	element_buffer.Unbind();
 	shader_program.DontUseThisProgram();
 }
